45cda507b2b0810cd4e1cb196f3e217e3e689573
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2025-09-26 23:15:44 +0300

    Added function definitions for the Stack chapter.

--- src/main/java/Array/Stack.java
+++ src/main/java/Array/Stack.java
@@ -38,30 +38,75 @@ public class Stack {
         return null;
     }
 
+    /**
+     * Write a method which checks if the array s is of the form $1^m2^m$ without counting
+     * any characters and using an external array based stack.
+     * <ul>
+     *     <li>You can assume the array consists of only numbers between 1 and 2 and there is at least one from each of them. </li>
+     *     <li>You are only allowed to use pop, push, isEmpty functions. </li>
+     *     <li>You should use one single external stack. </li>
+     *     <li>You can not modify {\bf s}.</li>
+     *     <li>Beware, you should return false for the strings 21, 1212, or 112212.</li>
+     * </ul>
+     */
     public boolean checkString(int[] s){
         return false;
     }
 
+    /**
+     * Write a method that compresses the stack by removing the duplicate items. Assume that the elements in the stack
+     * are sorted. You are only allowed to use pop, push, isEmpty functions.  (Hint: Use external stack).
+     */
     public void compress(){
     }
 
+    /**
+     * Write a method which copies all the elements of the src stack and inserts to the stack at position index. The
+     * bottom element in the stack has index 0 as usual. You are not allowed to use pop, push, isEmpty functions. You
+     * can assume the destination stack has enough space for insertion. Your method should run in ${\cal O}(N)$ time.
+     */
     public void copyPaste(Stack src, int index){
     }
 
+    /**
+     * Write a function that inserts a new integer after the largest element of the stack. You are not allowed to use
+     * any stack methods, just attributes, constructors, getters and setters.
+     */
     public void insertAfterLargest(int newValue){
     }
 
+    /**
+     * Write a static method using an external stack (only one external stack is allowed) that determines if an integer
+     * array is balanced or not. A number $k$ less than 10 is balanced with the number $10 + k$. For example, the array
+     * 2, 3, 13, 12, 4, 14 is balanced, whereas 5, 15, 4, 3, 14, 13 not. You are not allowed to use any stack attributes
+     * such as N, top, array etc.
+     */
     static boolean isBalanced(int[] a){
         return false;
     }
 
+    /**
+     * Write a method which pops the $k$'th element from the top and returns it. pop(1) is equal to the original pop,
+     * that is, the first element at the top has index 1. You are not allowed to use any stack methods and any external
+     * stacks, just attributes, constructors, getters and setters.
+     */
     Element pop(int k){
         return null;
     }
 
+    /**
+     * Write the method which pushes data as the $k$'th element from the top. push(1, data) is equal to the original
+     * push, that is, the first element at the top has index 1. You are not allowed to use any stack methods and any
+     * external stacks. You are allowed to use attributes, constructors, getters and setters.
+     */
     public void push(int k, int data){
     }
 
+    /**
+     * Write a method which removes only the even indexed (2, 4, . . .) elements from the stack. The first element at
+     * the bottom has index 1. You are only allowed to use pop, push, isEmpty functions (Hint: Use external stack). You
+     * are not allowed to use any stack attributes such as N, top, array etc.
+     */
     void removeEvenIndexed(){
     }
 
--- src/main/java/List/Stack.java
+++ src/main/java/List/Stack.java
@@ -26,30 +26,78 @@ public class Stack {
         return topNode;
     }
 
+    /**
+     * Write a new stack constructor which constructs a new stack by copying elements from stack $s$ indexed between $p$
+     * and $q$ (both inclusive).
+     * <ul>
+     *     <li>Assume that $q \geq p$. </li>
+     *     <li>The elements of the constructed stack should be new, not the elements of {\bf s}. </li>
+     *     <li>The first element at the top of the stack {\bf s} has index 0. </li>
+     *     <li>You are not allowed to use any stack methods and any external stacks. </li>
+     *     <li>You are allowed to use attributes, constructors, getters and setters.</li>
+     *     <li>You can not modify {\bf s}.</li>
+     * </ul>
+     */
     public Stack(Stack s, int p, int q){
     }
 
+    /**
+     * Write the method which adds all elements indexed from p to q of the stack s to the top of the original stack.
+     * Your stack should contain new nodes, not the nodes in the stack s. The element at the top of the stack has index
+     * 1. You are not allowed to use any stack methods and any external stacks. You are allowed to use attributes,
+     * constructors, getters and setters.
+     */
     void addToStack(Stack s, int p, int q){
     }
 
+    /**
+     * Write the method which checks if the array $s$ is of the form $1^m2^n3^n4^m$ without counting any characters and
+     * using an external list based stack. You can assume the array consists of only numbers between 1 and 4, they are
+     * in that order, and there is at least one from each of them. You are only allowed to use pop, push, isEmpty
+     * functions. You should use a single external stack.
+     */
     public boolean checkString(int[] s){
         return false;
     }
 
+    /**
+     * Write a method which pops the $k$'th element from the top and returns it. pop(1) is equal to the original pop,
+     * that is, the first element at the top has index 1. You are not allowed to use any stack methods and any external
+     * stacks, just attributes, constructors, getters and setters.
+     */
     Node pop(int k){
         return null;
     }
 
+    /**
+     * Write the method which pops $k$ elements from the bottom and return them as a singly linked list. You are not
+     * allowed to use any stack methods and any external stacks. You are allowed to use attributes, constructors,
+     * getters and setters.
+     */
     LinkedList popBottomK(int k){
         return new LinkedList();
     }
 
+    /**
+     * Write the method which pushes data as the $k$'th element from the top. push(1, data) is equal to the original
+     * push, that is, the first element at the top has index 1. You are not allowed to use any stack methods and any
+     * external stacks. You are allowed to use attributes, constructors, getters and setters.
+     */
     public void push(int k, int data){
     }
 
+    /**
+     * Write a function which removes only the $K$ bottom elements of the stack. You are only allowed to use pop, push,
+     * isEmpty functions. You can use an external stack.
+     */
     void removeBottomK(int K){
     }
 
+    /**
+     * Write a method which removes only the odd indexed (1, 3, $\ldots$) elements from the stack. The first element at
+     * the bottom has index 1. You are only allowed to use pop, push, isEmpty functions (Hint: Use external stack). You
+     * are not allowed to use any stack attributes such as $N$, $top$, $array$ etc.
+     */
     public void removeOddIndexed(){
     }
 
