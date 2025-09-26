4e1607e3875b6604b7952e93220e618064749cb0
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2025-09-26 23:32:22 +0300

    Added function definitions for the Queue chapter.

--- src/main/java/Array/Queue.java
+++ src/main/java/Array/Queue.java
@@ -41,33 +41,81 @@ public class Queue {
         return null;
     }
 
+    /**
+     * Write another constructor method which constructs a new array based queue by adding the elements in the $list$
+     * of queues one by one.  So, the first $k$ elements of the original queue will be constructed with the first
+     * elements of the $k$ queues in the list; the second $k$ elements of the original queue will be constructed with
+     * the second elements of the $k$ queues in the list etc. The elements from queues should be recreated (not copied
+     * from the queues). You are not allowed to use enqueue, dequeue, isEmpty functions.
+     */
     public Queue(Queue[] list){
     }
 
+    /**
+     * Write a method which copies all the elements of the src queue and inserts to the queue at position index. You are
+     * not allowed to use enqueue, dequeue, isEmpty functions. You can assume the destination queue has enough space for
+     * insertion. Your method should run in ${\cal O}(N)$ time. Hint: Start by counting the number of positions to shift
+     * for opening up space for the elements of src.
+     */
     public void copyPaste(Queue src, int index){
     }
 
+    /**
+     * Write a method which cuts all the elements between indexes p and q from the original queue and inserts at the end
+     * to the dest queue. You are not allowed to use enqueue, dequeue, isEmpty functions. You can assume the destination
+     * queue has enough space for insertion. Your method should run in ${\cal O}(N)$ time.
+     */
     void cutPaste(Queue dest, int p, int q){
     }
 
+    /**
+     * Write a method which dequeues data as the $k$'th element from the first. dequeue(1) is equal to the original
+     * dequeue, that is, the first element has index 1. You are not allowed to use any queue methods and any external
+     * structures (arrays, queues, trees, etc). You are allowed to use attributes, constructors, getters and setters.
+     */
     Element dequeue(int k){
         return null;
     }
 
+    /**
+     * Write a method which removes and returns the second item from the queue. Your methods should run in ${\cal O}$(1)
+     * time. Do not use any class or external methods except isEmpty().
+     */
     Element dequeue2nd(){
         return null;
     }
 
+    /**
+     * Write a function that creates and returns a new queue by removing even indexed elements from the original queue
+     * and inserting into the newly created queue. The first node has index 1. You are not allowed to use any queue or
+     * linked list methods, just attributes, constructors, getters and setters.
+     */
     Queue divideQueue(){
         return null;
     }
 
+    /**
+     * Write a function that inserts a new element after the largest element of the queue. Write the function for array
+     * implementation. You are not allowed to use any queue methods, just attributes, constructors, getters and setters.
+     */
     void insertAfterLargest (int data){
     }
 
+    /**
+     * Write the method which removes only the odd indexed (1, 3, . . .) elements from the queue. The first element has
+     * index 1. You are only allowed to use enqueue, dequeue, isEmpty functions. {\bf You should use external queue}.
+     * You are not allowed to use any queue attributes such as first, last, array etc.
+     */
     void removeOddIndexed(){
     }
 
+    /**
+     * Write a method where {$k$} is a non-negative integer representing the number of positions to rotate the queue by,
+     * which is implemented using an array. After the rotation, the first element of the queue should move to the back
+     * {$k$} times, and the order of the other elements should shift accordingly. You are not allowed to use the
+     * enqueue(), dequeue(), or peek() methods. The solution should rotate the array in {$O(N)$} time, where {$N$} is
+     * the number of elements in the queue. Assume that $k \leq N$.
+     */
     public void rotateQueue(int k){
     }
 
--- src/main/java/List/Queue.java
+++ src/main/java/List/Queue.java
@@ -34,35 +34,78 @@ public class Queue {
         return result;
     }
 
+    /**
+     * Write another constructor method which constructs a new list based queue by concatenating all elements in the list
+     * of queues in order. The elements from queues should be recreated (not copied from the queues). You are not
+     * allowed to use enqueue, dequeue, isEmpty functions.
+     */
     Queue(Queue[] list){
     }
 
+    /**
+     * Write a method which dequeues data as the $k$'th element from the first. dequeue(1) is equal to the original
+     * dequeue, that is, the first element has index 1. You are not allowed to use any queue methods and any external
+     * structures (arrays, queues, trees, etc). You are allowed to use attributes, constructors, getters and setters.
+     */
     Node dequeue(int k){
         return null;
     }
 
+    /**
+     * Write a function that creates and returns a new queue by removing even indexed elements from the original queue
+     * and inserting into the newly created queue. The first node has index 1. You are not allowed to use any queue or
+     * linked list methods, just attributes, constructors, getters and setters.
+     */
     Queue divideQueue(){
         return null;
     }
 
+    /**
+     * Write a method which constructs an array of list based queues by dividing the original queue into k equal parts.
+     * The first, second, $\ldots$, $k$'th element of the original queue will be the first element of the first, second,
+     * $\ldots$, $k$'th output queues, etc. The elements of the output queues should be recreated (not copied from the
+     * original queue). You are not allowed to use enqueue, dequeue, isEmpty functions.
+     */
     Queue[] divideQueue(int k){
         return null;
     }
 
+    /**
+     * Write a method where the method returns the minimum number in a queue. Do not use any class or external methods
+     * except isEmpty().
+     */
     public int minimum(){
         return 0;
     }
 
+    /**
+     * Write a method that returns the maximum number in a queue.
+     */
     public int maximum(){
         return 0;
     }
 
+    /**
+     * Write a method which removes all elements in the queues in the $list$ from the original queue. You are not
+     * allowed to use enqueue, dequeue, isEmpty functions.
+     */
     public void removeAll(Queue[] list){
     }
 
+    /**
+     * Write a method given a queue that is implemented as a linked list. The method should reverse the order of
+     * elements in the queue without using the queue’s enqueue(), dequeue(), or peek() methods. You must directly
+     * manipulate the underlying linked list of the queue to achieve the reversal.
+     */
     public void reverseQueue(){
     }
 
+    /**
+     * Write a method which simulates how the queue goes in an hypothetical country, which we all aware of.
+     * {\em indexOfNonbribers} is an increasing order sorted array showing the indexes of the people who have not bribed
+     * the officer in the queue. The method will send these people to the end of the queue in their respective order.
+     * The first element in the queue has the index 0.
+     */
     public void thisMustChange(int[] indexOfNonbribers){
     }
 }
